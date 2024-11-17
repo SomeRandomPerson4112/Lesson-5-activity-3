fe83b898732a40d6279c84c951dab2e9fdc0d154
SomeRandomPerson4112 <ayaangadani@gmail.com>
2024-11-17 12:01:09 +0500

    main.019348937489578390938763825976832975629867298766293864239768938562937862786327683995662938672938676962837

---
+++ Greater than or less than.py
@@ -0,0 +1,7 @@
+number=int(input("Enter a number: "))
+if number>15:
+    print("The number is greater than 15")
+    print("i am in if block")
+else:
+    print("The number is less than or equal to 15")
+    print("i am in else block")
